0bb417ed647f78fc30647ed6f9f6c205c9fde26b
Dynamo <zigagorenc1@gmail.com>
2024-03-23 18:03:39 +0000

    Zadnji commit z vsemi generiranimi datotekami

--- main.i
+++ main.i
@@ -758,10 +758,13 @@ int main() {
 
 
 
-    printf("Program se izvaja v 64-bitnem načinu.\n");
-
-
-
+    if (sizeof(void*) == 4) {
+        printf("Program se izvaja v 32-bitnem načinu.\n");
+    } else if (sizeof(void*) == 8) {
+        printf("Program se izvaja v 64-bitnem načinu.\n");
+    } else {
+        printf("Nepričakovana velikost kazalca, ni mogoče določiti načina.\n");
+    }
 
 
     max_num = findMax(a, b);
--- main.s
+++ main.s
@@ -5,20 +5,20 @@
 findMax:
 .LFB0:
 	.cfi_startproc
-	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
-	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
-	call	__x86.get_pc_thunk.ax
-	addl	$_GLOBAL_OFFSET_TABLE_, %eax
-	movl	12(%ebp), %edx
-	movl	8(%ebp), %eax
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
 	cmpl	%eax, %edx
 	cmovge	%edx, %eax
-	popl	%ebp
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
+	popq	%rbp
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE0:
@@ -30,10 +30,10 @@ findMax:
 	.string	"%d"
 .LC2:
 	.string	"Vnesite drugo \305\241tevilo: "
-	.align 4
+	.align 8
 .LC3:
 	.string	"Program se izvaja v 64-bitnem na\304\215inu."
-	.align 4
+	.align 8
 .LC4:
 	.string	"Najve\304\215je \305\241tevilo med %d in %d je: %d\n"
 	.text
@@ -42,111 +42,80 @@ findMax:
 main:
 .LFB1:
 	.cfi_startproc
-	leal	4(%esp), %ecx
-	.cfi_def_cfa 1, 0
-	andl	$-16, %esp
-	pushl	-4(%ecx)
-	pushl	%ebp
-	movl	%esp, %ebp
-	.cfi_escape 0x10,0x5,0x2,0x75,0
-	pushl	%ebx
-	pushl	%ecx
-	.cfi_escape 0xf,0x3,0x75,0x78,0x6
-	.cfi_escape 0x10,0x3,0x2,0x75,0x7c
-	subl	$16, %esp
-	call	__x86.get_pc_thunk.bx
-	addl	$_GLOBAL_OFFSET_TABLE_, %ebx
-	movl	%gs:20, %eax
-	movl	%eax, -12(%ebp)
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
 	xorl	%eax, %eax
-	subl	$12, %esp
-	leal	.LC0@GOTOFF(%ebx), %eax
-	pushl	%eax
-	call	printf@PLT
-	addl	$16, %esp
-	subl	$8, %esp
-	leal	-24(%ebp), %eax
-	pushl	%eax
-	leal	.LC1@GOTOFF(%ebx), %eax
-	pushl	%eax
-	call	__isoc99_scanf@PLT
-	addl	$16, %esp
-	subl	$12, %esp
-	leal	.LC2@GOTOFF(%ebx), %eax
-	pushl	%eax
-	call	printf@PLT
-	addl	$16, %esp
-	subl	$8, %esp
-	leal	-20(%ebp), %eax
-	pushl	%eax
-	leal	.LC1@GOTOFF(%ebx), %eax
-	pushl	%eax
-	call	__isoc99_scanf@PLT
-	addl	$16, %esp
-	subl	$12, %esp
-	leal	.LC3@GOTOFF(%ebx), %eax
-	pushl	%eax
-	call	puts@PLT
-	addl	$16, %esp
-	movl	-20(%ebp), %edx
-	movl	-24(%ebp), %eax
-	subl	$8, %esp
-	pushl	%edx
-	pushl	%eax
-	call	findMax
-	addl	$16, %esp
-	movl	%eax, -16(%ebp)
-	movl	-20(%ebp), %edx
-	movl	-24(%ebp), %eax
-	pushl	-16(%ebp)
-	pushl	%edx
-	pushl	%eax
-	leal	.LC4@GOTOFF(%ebx), %eax
-	pushl	%eax
-	call	printf@PLT
-	addl	$16, %esp
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
 	movl	$0, %eax
-	movl	-12(%ebp), %edx
-	subl	%gs:20, %edx
+	call	printf@PLT
+	leaq	-20(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	movl	-16(%rbp), %edx
+	movl	-20(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	findMax
+	movl	%eax, -12(%rbp)
+	movl	-16(%rbp), %edx
+	movl	-20(%rbp), %eax
+	movl	-12(%rbp), %ecx
+	movl	%eax, %esi
+	leaq	.LC4(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
 	je	.L5
-	call	__stack_chk_fail_local
+	call	__stack_chk_fail@PLT
 .L5:
-	leal	-8(%ebp), %esp
-	popl	%ecx
-	.cfi_restore 1
-	.cfi_def_cfa 1, 0
-	popl	%ebx
-	.cfi_restore 3
-	popl	%ebp
-	.cfi_restore 5
-	leal	-4(%ecx), %esp
-	.cfi_def_cfa 4, 4
+	leave
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE1:
 	.size	main, .-main
-	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
-	.globl	__x86.get_pc_thunk.ax
-	.hidden	__x86.get_pc_thunk.ax
-	.type	__x86.get_pc_thunk.ax, @function
-__x86.get_pc_thunk.ax:
-.LFB2:
-	.cfi_startproc
-	movl	(%esp), %eax
-	ret
-	.cfi_endproc
-.LFE2:
-	.section	.text.__x86.get_pc_thunk.bx,"axG",@progbits,__x86.get_pc_thunk.bx,comdat
-	.globl	__x86.get_pc_thunk.bx
-	.hidden	__x86.get_pc_thunk.bx
-	.type	__x86.get_pc_thunk.bx, @function
-__x86.get_pc_thunk.bx:
-.LFB3:
-	.cfi_startproc
-	movl	(%esp), %ebx
-	ret
-	.cfi_endproc
-.LFE3:
-	.hidden	__stack_chk_fail_local
 	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
 	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
