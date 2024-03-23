78383a60a0359c68a5404c4f640e38ad60b3a3b3
Dynamo <zigagorenc1@gmail.com>
2024-03-23 18:01:24 +0000

    Popravek v demo programu, ki pravilno identificira arhitekturo

--- main.c
+++ main.c
@@ -19,11 +19,14 @@ int main() {
     scanf("%d", &b);
 
     // Vejitvena predprocesorska direktiva
-#if defined(__LP64__)
-    printf("Program se izvaja v 64-bitnem načinu.\n");
-#else
-    printf("Program se izvaja v 32-bitnem načinu.\n");
-#endif
+    // Preverjanje velikosti kazalca
+    if (sizeof(void*) == 4) {
+        printf("Program se izvaja v 32-bitnem načinu.\n");
+    } else if (sizeof(void*) == 8) {
+        printf("Program se izvaja v 64-bitnem načinu.\n");
+    } else {
+        printf("Nepričakovana velikost kazalca, ni mogoče določiti načina.\n");
+    }
 
     // Klic funkcije in uporaba makra
     max_num = findMax(a, b);
